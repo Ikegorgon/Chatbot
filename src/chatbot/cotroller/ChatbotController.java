4098cd68f8247721ccf019ddf8effd068a52f82b
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-10-17 13:34:33 -0600

    Beginning of  ChatbotController made with imping of .model and .view

---
+++ src/chatbot/cotroller/ChatbotController.java
@@ -0,0 +1,12 @@
+package chatbot.cotroller;
+
+import chatbot.model.*;
+import chatbot.view.*;
+
+public class ChatbotController {
+	Chatbot chatbot = new Chatbot();
+	PopupDisplay display = new PopupDisplay();
+	public void start() {
+		
+	}
+}

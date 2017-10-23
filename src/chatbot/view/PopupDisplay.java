5f5e8fb3f3c7fe203aaaa3316982be4de8f801a2
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-10-23 11:47:14 -0600

    Added imageIcon and windowTitle variables for PopupDisplay

--- src/chatbot/view/PopupDisplay.java
+++ src/chatbot/view/PopupDisplay.java
@@ -1,8 +1,15 @@
 package chatbot.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class PopupDisplay {
+	private ImageIcon icon;
+	private String windowTitle;
+	public PopupDisplay() {
+		icon = new ImageIcon(getClass().getResource("images/trump.png"));
+		windowTitle = "Chatbot says ";
+	}
 	public void displayText (String sample) {
 		JOptionPane.showMessageDialog(null, sample);
 	}

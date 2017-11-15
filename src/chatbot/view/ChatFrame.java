4bfb62d16bdc6bacb7cae9fb51d625ebc775c556
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-11-15 12:22:15 -0700

    ChatFrame finished

--- src/chatbot/view/ChatFrame.java
+++ src/chatbot/view/ChatFrame.java
@@ -4,5 +4,23 @@ import chatbot.controller.*;
 import javax.swing.*;
 
 public class ChatFrame extends JFrame {
+	private ChatbotController app;
+	private ChatPanel appPanel;
+	public ChatFrame(ChatbotController app) {
+		super();
+		this.app = app;
+		appPanel = new ChatPanel(app);
+		setupFrame();
+	}
+	private void setupFrame() {
+		this.setContentPane(appPanel);
+		this.setTitle("Chatbot");
+		this.setSize(1000, 1000);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 	
+	public ChatbotController getBaseController() {
+		return app;
+	}
 }
